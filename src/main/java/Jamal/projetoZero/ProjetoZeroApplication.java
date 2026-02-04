5c2ccbf566a73bc5061eb59a1f52c411a615cb4a
btpjamal <jvbraga2608@gmail.com>
2026-02-04 20:40:06 -0300

    first commit

---
+++ src/main/java/Jamal/projetoZero/ProjetoZeroApplication.java
@@ -0,0 +1,13 @@
+package Jamal.projetoZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jamal/projetoZero/ProjetoZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package Jamal.projetoZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
