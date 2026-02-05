a0d68e2dc2a6de2030f78a959aa37b74c9c03c9e
btpjamal <jvbraga2608@gmail.com>
2026-02-05 00:11:32 -0300

    primeira mensagem pela rota controller

---
+++ src/main/java/Jamal/projetoZero/Controller.java
@@ -0,0 +1,18 @@
+package Jamal.projetoZero;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// anotações necessárias para um Controller
+@RestController
+@RequestMapping
+public class Controller {
+
+    // pegar informações
+    @GetMapping("/boasvindas") // -> localhost:8080/boasvindas
+    public String boasVindas(){
+        return "Primeira mensagem nessa rota";
+    }
+
+}
